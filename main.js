1f6c561c22eb4ff8752d576dec36dc72b1887632
turtleprince <81336142+turtleprince@users.noreply.github.com>
2022-01-14 18:25:43 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+var first_name = "um";
+console.log(first_name);
+var last_name = "rico";
+console.log(last_name);
+var full_name =  (first_name.concat(last_name));
+console.log(full_name);
+var minimum = Math.min(1,2);
+console.log(minimum);
